7e201ac1caec203bbb20e0b4ed285aca24150bf5
Miguel López <miguelopezv@gmail.com>
2023-03-20 17:22:40 -0500

    update constants

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import numpy as np
 import tensorflow as tf
 
-from constants import SHAKESPEARE_URL, BATCH_SIZE, EMBEDDING_DIM, RNN_UNITS, BUFFER_SIZE
+from constants import SHAKESPEARE_URL, BATCH_SIZE, EMBEDDING_DIM, RNN_UNITS, BUFFER_SIZE, SEQ_LENGTH
 from helpers import encode, split_input_target, build_model
 
 tf.config.set_visible_devices([], 'GPU')
@@ -15,22 +14,18 @@ text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
 
 vocab = sorted(set(text))
 char2idx = {char: i for i, char in enumerate(vocab)}
-idx2npArr = np.array(vocab)
-
+# idx2npArr = np.array(vocab)
 text_as_int = encode(char2idx, text)
 
 # divide dataset into batches
-seq_length = 100
 text_dataset = tf.data.Dataset.from_tensor_slices(text_as_int)
-sequences = text_dataset.batch(seq_length + 1, drop_remainder=True)
+sequences = text_dataset.batch(SEQ_LENGTH + 1, drop_remainder=True)
 
 # Transform each batch into input and target
 dataset = sequences.map(split_input_target)
 
-VOCAB_SIZE = len(vocab)
-
 # Create batches to train the model
 data = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
 
-model = build_model(VOCAB_SIZE, EMBEDDING_DIM, RNN_UNITS, BATCH_SIZE)
+model = build_model(len(vocab), EMBEDDING_DIM, RNN_UNITS, BATCH_SIZE)
 model.summary()
