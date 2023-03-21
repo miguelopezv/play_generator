ea49dfbc4b12e69061464c72b18596cf4a3e37e6
Miguel López <miguelopezv@gmail.com>
2023-03-20 21:57:29 -0500

    update main file

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import numpy as np
 import tensorflow as tf
 
 from constants import SHAKESPEARE_URL, BATCH_SIZE, EMBEDDING_DIM, RNN_UNITS, BUFFER_SIZE, SEQ_LENGTH
-from helpers import encode, split_input_target, build_model
-
-tf.config.set_visible_devices([], 'GPU')
+from helpers import encode, split_input_target, build_model, loss, checkpoint_callback
 
 res = input('We are going to train our model with a default play, if ok press ENTER otherwise write \'custom\': ')
 while res not in ['', 'custom']:
@@ -14,6 +13,7 @@ text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
 
 vocab = sorted(set(text))
 char2idx = {char: i for i, char in enumerate(vocab)}
+idx2char = np.array(vocab)
 text_as_int = encode(char2idx, text)
 
 # divide dataset into batches
@@ -25,6 +25,16 @@ dataset = sequences.map(split_input_target)
 
 # Create batches to train the model
 data = dataset.shuffle(BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True)
-
 model = build_model(len(vocab), EMBEDDING_DIM, RNN_UNITS, BATCH_SIZE)
-model.summary()
+model.compile(optimizer='adam', loss=loss)
+
+# Comment this line after the model has been trained
+history = model.fit(data, epochs=1, callbacks=[checkpoint_callback])
+
+# With model already trained, use this to recreate the model expecting 1 input
+# model = build_model(len(vocab), EMBEDDING_DIM, RNN_UNITS, batch_size=1)
+# model.load_weights(tf.train.latest_checkpoint(CHECKPOINTS_DIR))
+# model.build(tf.TensorShape([1, None]))
+#
+# inp = input("Type a starting string: ")
+# print(generate_text(model, inp, char2idx, idx2char))
