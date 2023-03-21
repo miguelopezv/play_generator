2f7077aad13534df056ace614111e12312ad81bd
Miguel López <miguelopezv@gmail.com>
2023-03-20 19:20:09 -0500

    update helpers.py

--- helpers.py
+++ helpers.py
@@ -1,5 +1,5 @@
 import numpy as np
-import tensorflow as tf
+from tensorflow import keras
 
 
 def encode(char2idx, text):
@@ -13,12 +13,17 @@ def split_input_target(chunk):
 
 
 def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
-    return tf.keras.Sequential([
-        tf.keras.layers.Embedding(vocab_size, embedding_dim,
-                                  batch_input_shape=[batch_size, None]),
-        tf.keras.layers.LSTM(rnn_units,
-                             return_sequences=True,
-                             stateful=True,
-                             recurrent_initializer='glorot_uniform'),
-        tf.keras.layers.Dense(vocab_size)
+    return keras.Sequential([
+        keras.layers.Embedding(vocab_size, embedding_dim,
+                               batch_input_shape=[batch_size, None]),
+        keras.layers.LSTM(rnn_units,
+                          # return_sequences=True,
+                          stateful=True,
+                          # recurrent_initializer='glorot_uniform'
+                          ),
+        keras.layers.Dense(vocab_size)
     ])
+
+
+def loss(labels, logits):
+    return keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
