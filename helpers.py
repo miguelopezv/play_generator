7d58d1502f8bdce0c99f48a62059918ae7c405d5
Miguel López <miguelopezv@gmail.com>
2023-03-20 21:57:19 -0500

    new helpers

--- helpers.py
+++ helpers.py
@@ -1,5 +1,10 @@
+import os
+
 import numpy as np
+import tensorflow as tf
 from tensorflow import keras
+
+from constants import CHECKPOINTS_DIR
 
 
 def encode(char2idx, text):
@@ -17,13 +22,45 @@ def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
         keras.layers.Embedding(vocab_size, embedding_dim,
                                batch_input_shape=[batch_size, None]),
         keras.layers.LSTM(rnn_units,
-                          # return_sequences=True,
+                          return_sequences=True,
                           stateful=True,
-                          # recurrent_initializer='glorot_uniform'
-                          ),
+                          recurrent_initializer='glorot_uniform'),
         keras.layers.Dense(vocab_size)
     ])
 
 
 def loss(labels, logits):
     return keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
+
+
+checkpoint_prefix = os.path.join(CHECKPOINTS_DIR, "ckpt_{epoch}")
+
+checkpoint_callback = keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_prefix,
+    save_weights_only=True)
+
+
+def generate_text(model, start_string, char2idx, idx2char):
+    num_generate = 800
+
+    input_eval = [char2idx[s] for s in start_string]
+    input_eval = tf.expand_dims(input_eval, 0)
+
+    text_generated = []
+
+    # Low temperatures results in more predictable text.
+    # Higher temperatures results in more surprising text.
+    temperature = 1.0
+
+    model.reset_states()
+    for i in range(num_generate):
+        predictions = model(input_eval)
+        predictions = tf.squeeze(predictions, 0)
+
+        predictions = predictions / temperature
+        predicted_id = tf.random.categorical(predictions, num_samples=1)[-1, 0].numpy()
+
+        input_eval = tf.expand_dims([predicted_id], 0)
+        text_generated.append(idx2char[predicted_id])
+
+    return start_string + ''.join(text_generated)
